0dddf3fc81208e85e8f8a0daa4d185ea19a6701c
osama6346 <osamaiqbal0986346@gmail.com>
2024-09-12 20:11:34 +0500

    Added new fields for the sensors

--- src/controllers/sensorController.js
+++ src/controllers/sensorController.js
@@ -9,33 +9,42 @@ exports.addSensorData = async (req, res) => {
 
   try {
     const site = await Site.findOne({ siteId });
-
     if (!site) {
       return res.status(404).send('Site not found');
     }
 
     const node = await Node.findOne({ nodeId, site: site._id });
-
     if (!node) {
       return res.status(404).send('Node not found');
     }
 
     for (const sensorData of sensorDataArray) {
-      let sensor = await Sensor.findOne({ sensorId: sensorData.sensorId, node: node._id });
+      const sensorDataToSave = {
+        sensorId: sensorData.sensorId,
+        ...(sensorData.xAxisVibrationSpeed !== undefined && { xAxisVibrationSpeed: sensorData.xAxisVibrationSpeed }),
+        ...(sensorData.yAxisVibrationSpeed !== undefined && { yAxisVibrationSpeed: sensorData.yAxisVibrationSpeed }),
+        ...(sensorData.zAxisVibrationSpeed !== undefined && { zAxisVibrationSpeed: sensorData.zAxisVibrationSpeed }),
+        ...(sensorData.chipTime && { chipTime: sensorData.chipTime }),
+        ...(sensorData.xAxisAngularVibrationAmplitude !== undefined && { xAxisAngularVibrationAmplitude: sensorData.xAxisAngularVibrationAmplitude }),
+        ...(sensorData.yAxisAngularVibrationAmplitude !== undefined && { yAxisAngularVibrationAmplitude: sensorData.yAxisAngularVibrationAmplitude }),
+        ...(sensorData.zAxisAngularVibrationAmplitude !== undefined && { zAxisAngularVibrationAmplitude: sensorData.zAxisAngularVibrationAmplitude }),
+        ...(sensorData.temperature !== undefined && { temperature: sensorData.temperature }),
+        ...(sensorData.xAxisVibrationDisplacement !== undefined && { xAxisVibrationDisplacement: sensorData.xAxisVibrationDisplacement }),
+        ...(sensorData.yAxisVibrationDisplacement !== undefined && { yAxisVibrationDisplacement: sensorData.yAxisVibrationDisplacement }),
+        ...(sensorData.zAxisVibrationDisplacement !== undefined && { zAxisVibrationDisplacement: sensorData.zAxisVibrationDisplacement }),
+        ...(sensorData.xAxisFrequencyVibrationFrequency !== undefined && { xAxisFrequencyVibrationFrequency: sensorData.xAxisFrequencyVibrationFrequency }),
+        ...(sensorData.yAxisFrequencyVibrationFrequency !== undefined && { yAxisFrequencyVibrationFrequency: sensorData.yAxisFrequencyVibrationFrequency }),
+        ...(sensorData.zAxisFrequencyVibrationFrequency !== undefined && { zAxisFrequencyVibrationFrequency: sensorData.zAxisFrequencyVibrationFrequency }),
+        ...(sensorData.windspeed !== undefined && { windspeed: sensorData.windspeed }),
+        ...(sensorData.winddirection !== undefined && { winddirection: sensorData.winddirection }),
+        ...(sensorData.latitude && { latitude: sensorData.latitude }),
+        ...(sensorData.longitude && { longitude: sensorData.longitude }),
+        node: node._id,
+      };
 
-      if (sensor) {
-        sensor.data.push(sensorData);
-        await sensor.save();
-      } else {
-        sensor = new Sensor({
-          sensorId: sensorData.sensorId,
-          data: [sensorData],
-          node: node._id,
-        });
-        await sensor.save();
-        node.sensors.push(sensor._id);
-        await node.save();
-      }
+      const newSensor = new Sensor(sensorDataToSave);
+      await newSensor.save();
+
     }
 
     if (versionNumber) {
@@ -49,13 +58,15 @@ exports.addSensorData = async (req, res) => {
       }
     }
 
-    return res.status(201).send('Sensor data saved');
+    return res.status(201).send('Sensor data saved successfully');
 
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 };
+
+
 
 exports.getSensors = async (req, res) => {
   try {
--- src/models/Sensor.js
+++ src/models/Sensor.js
@@ -1,13 +1,27 @@
 const mongoose = require('mongoose');
 
 const SensorSchema = new mongoose.Schema({
-  sensorId: String,
-  data: [mongoose.Schema.Types.Mixed],
+  sensorId: { type: String, required: true },
+  xAxisVibrationSpeed: { type: Number, required: false },
+  yAxisVibrationSpeed: { type: Number, required: false },
+  zAxisVibrationSpeed: { type: Number, required: false },
+  chipTime: { type: Date, required: false },
+  xAxisAngularVibrationAmplitude: { type: Number, required: false },
+  yAxisAngularVibrationAmplitude: { type: Number, required: false },
+  zAxisAngularVibrationAmplitude: { type: Number, required: false },
+  temperature: { type: Number, required: false },
+  xAxisVibrationDisplacement: { type: Number, required: false },
+  yAxisVibrationDisplacement: { type: Number, required: false },
+  zAxisVibrationDisplacement: { type: Number, required: false },
+  xAxisFrequencyVibrationFrequency: { type: Number, required: false },
+  yAxisFrequencyVibrationFrequency: { type: Number, required: false },
+  zAxisFrequencyVibrationFrequency: { type: Number, required: false },
+  windspeed: { type: Number, required: false },
+  winddirection: { type: Number, required: false },
+  latitude: { type: String, required: false },
+  longitude: { type: String, required: false },
   node: { type: mongoose.Schema.Types.ObjectId, ref: 'Node' }, 
-  timestamp: {
-    type: Date,
-    default: Date.now,
-  },
+  timestamp: { type: Date, default: Date.now },
 });
 
 const Sensor = mongoose.model('Sensor', SensorSchema);
